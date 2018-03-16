ee9d26c8c11d0c089238f367b7ce679b9693cb98
jshi <shijian0602@hotmail.com>
2018-03-16 20:51:51 +0800

    updated portfolio details

--- src/Data.js
+++ src/Data.js
@@ -9,21 +9,21 @@ export const PORTFOLIO = [
         "id": "act",
         "img" : actImg,
         "title": "act.org.nz",
-        "intro" : "act.org.nz is a wordpress website using HTML, CSS, PHP and JavaScript for template, also applied grid system to adapt a range of devices. It is also intergrated with CiviCRM to manage contacts",
+        "intro" : "act.org.nz is a WordPress website using HTML, CSS, PHP and JavaScript for the template. I applied a grid system to ensure it displays well on a range of devices. It is integrated with CiviCRM to manage contacts.",
         "skills": ["HTML5 - audio, video, canvas","CSS3 - preprocessed with SASS","PHP","JavaScript", "Ressponsive Website Design"]
     },
     {
         "id": "iea",
         "img": ieaImg,
         "title": "IEANZ",
-        "intro": "Website developed for IEANZ. Interpreted and executed designs as requested by clients then eveloped and enhanced the site. Maintained and updated HTML/CSS templates on a regular basis and as required.",
+        "intro": "Website developed for IEANZ. Interpreted and executed designs as requested by clients, then enveloped and enhanced the site. Maintained and updated HTML/CSS templates on a regular basis and as required.",
         "skills": ["UI/UX Design", "HTML5 - audio, video","CSS3 - preprocessed with SASS", "PHP"]
     },
     {
         "id": "eroll",
         "img": inrollImg,
         "title": "Electroal Roll",
-        "intro": "A database web application in PHP (Laravel framework) and MySQL to enable my client’s non-professional IT users and volunteers to access and update a copy of the New Zealand electoral roll. The electoral role is a regulated database complied by the New Zealand Electoral Commission and contains data on each of New Zealand’3,000,000 plus electors.",
+        "intro": "A database web application in PHP (Laravel framework) and MySQL to enable my client’s non-professional IT users and volunteers to access and update a copy of the New Zealand electoral roll. The electoral roll is a regulated database complied by the New Zealand Electoral Commission and contains data on each of New Zealand’s 3,000,000 plus electors.",
         "skills": ["HTML5", "CSS3", "AngularJS", "PHP - Laravel", "MySQL"]
     },
     {
@@ -37,14 +37,14 @@ export const PORTFOLIO = [
         "id": "js",
         "img": grabImg,
         "title": "Javascript Module",
-        "intro": "A ReactJS single-page module to collect donation that can be reused for other projects",
+        "intro": "A ReactJS single-page module to collect donations. The module can be reused for other projects.",
         "skills": ["HTML5","CSS3 - preprocessed with SASS", "React.JS"]
     },
     {
         "id": "lightwork",
         "img": lightworkImg,
         "title": "Lightwork Marking",
-        "intro": "I spent a summer working as a Java/PHP programmer on Lightwork project which is a Java application used for synchronising/updating Moodle server information through RESTful APIs.",
+        "intro": "I spent a summer working as a Java/PHP programmer on the Lightwork project at Massey University. Lightwork is a Java application used for synchronising/updating Moodle server information through RESTful APIs.",
         "skills": ["Java", "PHP"]
     }
 ]
